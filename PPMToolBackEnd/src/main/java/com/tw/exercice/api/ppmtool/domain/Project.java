f0f333d20f66521ba72b832d096c2192caa7e712
Rita Moraes <ritac.moraes@gmail.com>
2018-12-03 11:39:12 -0200

    Tratamento de erro no controller

--- PPMToolBackEnd/src/main/java/com/tw/exercice/api/ppmtool/domain/Project.java
+++ PPMToolBackEnd/src/main/java/com/tw/exercice/api/ppmtool/domain/Project.java
@@ -1,7 +1,11 @@
 package com.tw.exercice.api.ppmtool.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,12 +14,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please, use 4 or 5 caracteres")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
