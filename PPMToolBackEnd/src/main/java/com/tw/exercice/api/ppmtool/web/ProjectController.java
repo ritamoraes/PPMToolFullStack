921b89c7e3d6b525f505dcc6a99dfc299ff402a1
Rita Moraes <ritac.moraes@gmail.com>
2018-12-05 18:18:21 -0200

    Custom Exceptions

--- PPMToolBackEnd/src/main/java/com/tw/exercice/api/ppmtool/web/ProjectController.java
+++ PPMToolBackEnd/src/main/java/com/tw/exercice/api/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.tw.exercice.api.ppmtool.web;
 
 import com.tw.exercice.api.ppmtool.domain.Project;
+import com.tw.exercice.api.ppmtool.services.MapValidationErrorService;
 import com.tw.exercice.api.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,16 +24,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
         Project projectSaved = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
